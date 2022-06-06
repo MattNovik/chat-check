16269c9c760b96e230d24a3c898c703f27015406
mattNov <matvej.novik@gmail.com>
2022-06-06 18:07:29 +0300

    first fix

---
+++ server/config.js
@@ -0,0 +1,5 @@
+// разрешенный источник
+export const ALLOWED_ORIGIN = 'http://localhost:3000'
+// адрес БД
+export const MONGODB_URI =
+  'mongodb+srv://lockdur:eIrTDAUBzWOJAY4o@cluster0.zprdb.mongodb.net/?retryWrites=true&w=majority'
